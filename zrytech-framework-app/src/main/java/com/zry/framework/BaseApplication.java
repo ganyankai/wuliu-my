0b6027f8920f5009460e3ace3747cfc459a76ce7
yuwei <wei.yu@zrytech.com>
2019-02-19 10:03:48 +0800

    合并冲突

--- zrytech-framework-app/src/main/java/com/zry/framework/BaseApplication.java
+++ zrytech-framework-app/src/main/java/com/zry/framework/BaseApplication.java
@@ -1,15 +1,24 @@
 package com.zry.framework;
 
 import com.zrytech.framework.base.base.BaseApp;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.servlet.MultipartConfigElement;
 
+
+@ServletComponentScan
 @SpringBootApplication
+@MapperScan(basePackages = "com.zry.framework.*.mapper")
+@EnableAspectJAutoProxy
+@EnableScheduling
 public class BaseApplication extends BaseApp {
 
 	public static void main(String[] args) {
