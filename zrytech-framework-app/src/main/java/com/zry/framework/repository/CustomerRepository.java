ad0661c3493e4cdc9e82a3c9ea6a719922a15343
yuwei <wei.yu@zrytech.com>
2019-02-26 10:19:05 +0800

    司机与压货人前台接口

--- zrytech-framework-app/src/main/java/com/zry/framework/repository/CustomerRepository.java
+++ zrytech-framework-app/src/main/java/com/zry/framework/repository/CustomerRepository.java
@@ -1,10 +1,26 @@
 package com.zry.framework.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.zry.framework.entity.Customer;
 
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
+	
+	/**
+	 * 修改启用状态
+	 * @author cat
+	 * 
+	 * @param id	司机或压货人Id
+	 * @param enabled	启用状态
+	 * @return
+	 */
+	@Modifying
+	@Query("update Customer set isActive = ?2 where id = ?1")
+	int updateIsActiveById(Integer id, Boolean enabled);
+	
+	
 }
