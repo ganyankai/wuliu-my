94a8495777f4ea6f50e53ae147d301a243213429
liuwang <wang.liu@zrytech.com>
2019-02-28 16:55:20 +0800

    更改包名为com.zrytech.framework

---
+++ zrytech-framework-app/src/main/java/com/zrytech/framework/BaseApplication.java
@@ -0,0 +1,43 @@
+package com.zrytech.framework;
+
+import com.zrytech.framework.base.base.BaseApp;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.servlet.MultipartConfigElement;
+
+
+@ServletComponentScan
+@SpringBootApplication
+@MapperScan({"com.zrytech.framework.*.mapper"})
+//@ComponentScan({"com.zrytech.framework.*.*", "com.zrytech.framework.*.*"})
+//@EnableJpaRepositories({"com.zrytech.framework.*.repository"})
+//@EntityScan({"com.zrytech.framework.*.entity"})
+@EnableAspectJAutoProxy
+@EnableScheduling
+public class BaseApplication extends BaseApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseApplication.class, args);
+	}
+
+	@Value("${upload.file.dir}")
+	private String updatefile;
+
+	@Bean
+	MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setLocation(updatefile);
+		return factory.createMultipartConfig();
+	}
+}
