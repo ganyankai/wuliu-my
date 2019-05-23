bfcc09f7e3b0be8bfd635e240e6ad2019790f0c2
yuwei <wei.yu@zrytech.com>
2019-05-23 18:37:33 +0800

    去掉审核状态

--- zrytech-framework-app/src/main/java/com/zrytech/framework/app/constants/CargoMatterConstants.java
+++ zrytech-framework-app/src/main/java/com/zrytech/framework/app/constants/CargoMatterConstants.java
@@ -8,9 +8,6 @@ public class CargoMatterConstants {
 	/** 报价单状态 */
 	public static final String CARGO_MATTER_STATUS = "cargo_matter_status";
 
-	/** 报价单状态:未认证 */
-	public static final String CARGO_MATTER_STATUS_UNCERTIFIED = "cargo_matter_status_uncertified";
-
 	/** 报价单状态:应标中 */
 	public static final String CARGO_MATTER_STATUS_RELEASE = "cargo_matter_status_release";
 
